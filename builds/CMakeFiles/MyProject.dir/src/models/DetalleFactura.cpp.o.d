faf8952bdf930fed3fb0eb115cad9a798b63c436
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-11-20 00:18:57 -0500

    cambios

---
+++ builds/CMakeFiles/MyProject.dir/src/models/DetalleFactura.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/MyProject.dir/src/models/DetalleFactura.cpp.o: \
+ /home/juan/Documentos/c++/src/models/DetalleFactura.cpp \
+ /usr/include/stdc-predef.h \
+ /home/juan/Documentos/c++/src/models/DetalleFactura.h
---
+++ builds/CMakeFiles/MyProject.dir/src/models/Stock.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/MyProject.dir/src/models/Stock.cpp.o: \
+ /home/juan/Documentos/c++/src/models/Stock.cpp \
+ /usr/include/stdc-predef.h /home/juan/Documentos/c++/src/models/Stock.h
